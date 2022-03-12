2ffa4b39abf250ca8fb8549a584c501b9b3d19a0
numanumaa <jacobaustin528@gmail.com>
2022-03-12 13:26:17 -0500

    implemented IPHub API and the ability to switch between the two APIs. general improvements, though still made out of hot glue, popsicle sticks, and hope.

--- AntiVPN/AntiVPN.cs
+++ AntiVPN/AntiVPN.cs
@@ -10,16 +10,23 @@ namespace VPNPlugin
     public class VPNPlugin : TerrariaPlugin
     {
         public override string Name => "AntiVPN";
+
         public override string Description => "Jayton repellent";
+
         public override string Author => "numanuma";
+
         public override Version Version => new Version(1, 0);
 
-        private static string[] ignoredIPs = { "10.", "127", "172.16.", "172.17.", "172.18.", "172.19.", "172.20.", "172.21.", "172.22.", "172.23.", 
-                                               "172.24.", "172.25.", "172.26", "172.27", "172.28", "172.29", "172.30", "172.31", "192.168"};
+        static readonly string[] ignoredIPs = { "10.", "127", "172.16.", "172.17.", "172.18.", "172.19.", "172.20.", "172.21.", "172.22.", "172.23.",
+                                                "172.24.", "172.25.", "172.26", "172.27", "172.28", "172.29", "172.30", "172.31", "192.168"};
 
-        private static bool toggleAntiVPN = true;
+        static readonly HttpClient client = new HttpClient();
 
-        private static readonly HttpClient client = new HttpClient();
+        static string responseString;
+
+        static bool usingIPdata = true;
+
+        static bool antiVPNtoggle = true;
 
         public VPNPlugin(Main game) : base(game)
         {
@@ -37,10 +44,10 @@ namespace VPNPlugin
         public override void Initialize()
         {
             ServerApi.Hooks.GameInitialize.Register(this, OnInitialize);
-            ServerApi.Hooks.ServerJoin.Register(this, OnJoinAsync);
+            ServerApi.Hooks.ServerJoin.Register(this, OnJoin);
         }
 
-        async void OnJoinAsync(JoinEventArgs args)
+        async void OnJoin(JoinEventArgs args)
         {
             bool isIgnored = false;
 
@@ -56,27 +63,58 @@ namespace VPNPlugin
                 }
             }
 
-            if (isIgnored || !toggleAntiVPN)
+            if (isIgnored || !antiVPNtoggle)
             {
                 return;
             }
+            
+            for (int i = 0; i < 2; i++)
+            {
+                try
+                {
+                    if (usingIPdata)
+                    {
+                        responseString = await client.GetStringAsync($"http://check.getipintel.net/check.php?ip={TShock.Players[args.Who].IP}&contact=jacobaustin.sec@gmail.com&flags=m");
+                        float.TryParse(responseString, out float responseResult);
+                        if (responseResult >= 0.90 && responseResult < 2) 
+                        {
+                            TShock.Players[args.Who].Disconnect("[AntiVPN] VPN connections are not permitted.");
+                        }
+                    }
+                    else
+                    {
+                        HttpRequestMessage requestMessage = BuildRequest(TShock.Players[args.Who].IP);
+                        HttpResponseMessage response = await client.SendAsync(requestMessage);
+                        responseString = await response.Content.ReadAsStringAsync();
+                        if (responseString.Contains("k\":1,"))
+                        {
+                            TShock.Players[args.Who].Disconnect("[AntiVPN] VPN connections are not permitted.");
+                        }
+                    }
 
-            try
-            {
-                var responseString = await client.GetStringAsync($"http://check.getipintel.net/check.php?ip={TShock.Players[args.Who].IP}&contact=jacobaustin.sec@gmail.com&flags=m");
-                
-                float.TryParse(responseString, out float responseResult);
+                    i++;
+                }
+                catch (Exception e)
+                {
+                    if (e is HttpRequestException)
+                    {
+                        Console.WriteLine($"[AntiVPN] An error occurred verifying {TShock.Players[args.Who].Name}'s IP:\n{e.Message}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"[AntiVPN] An error occurred verifying {TShock.Players[args.Who].Name}'s IP:\n{e}");
+                    }
 
-                if (responseResult >= 0.90)
-                    TShock.Players[args.Who].Disconnect("[AntiVPN] VPN connections are not permitted.");
-            }
-            catch (HttpRequestException e)
-            {
-                Console.WriteLine($"[AntiVPN] An error occurred verifying {TShock.Players[args.Who].Name}'s IP:\n{e.Message}");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"[AntiVPN] An error occurred verifying {TShock.Players[args.Who].Name}'s IP:\n{e}");
+                    if (i == 0)
+                    {
+                        usingIPdata = !usingIPdata;
+                        Console.WriteLine($"[AntiVPN] Attempting alternative VPN lookup using {HostCheck()}...");
+                    }
+                    else
+                    {
+                        Console.WriteLine("[AntiVPN] Both APIs attempted; giving up.");
+                    }
+                }
             }
         }
 
@@ -87,18 +125,41 @@ namespace VPNPlugin
             switch (subcmd)
             {
                 case "toggle":
-                    toggleAntiVPN = !toggleAntiVPN;
-                    args.Player.SendSuccessMessage($"AntiVPN is now {((toggleAntiVPN) ? "enabled" : "disabled")}.");
+                    antiVPNtoggle = !antiVPNtoggle;
+                    args.Player.SendSuccessMessage($"AntiVPN is now {((antiVPNtoggle) ? $"enabled; using {HostCheck()}" : "disabled")}.");
                     break;
 
                 case "status":
-                    args.Player.SendSuccessMessage($"AntiVPN is currently {((toggleAntiVPN) ? "enabled" : "disabled")}.");
+                    args.Player.SendSuccessMessage($"AntiVPN is currently {((antiVPNtoggle) ? $"enabled; using {HostCheck()}" : "disabled")}.");
+                    break;
+
+                case "switchapi":
+                    usingIPdata = !usingIPdata;
+                    args.Player.SendSuccessMessage($"AntiVPN API switched to {HostCheck()}.");
                     break;
 
                 default:
-                    args.Player.SendErrorMessage("Invalid syntax! Proper syntax: {0}antivpn [status, toggle]", TShock.Config.Settings.CommandSpecifier);
+                    args.Player.SendErrorMessage("Invalid syntax! Proper syntax: {0}antivpn [status, switchapi, toggle]", TShock.Config.Settings.CommandSpecifier);
                     break;
             }
+        }
+
+        string HostCheck()
+        {
+            string host = usingIPdata ? "ipdata" : "IPHub";
+            return host;
+        }
+
+        HttpRequestMessage BuildRequest(string IP)
+        {
+            HttpRequestMessage requestMessage = new HttpRequestMessage
+            {
+                RequestUri = new Uri($"http://v2.api.iphub.info/ip/{IP}"),
+                Method = HttpMethod.Get
+            };
+            requestMessage.Headers.Add("X-Key", "MTcwMTc6VG5YWmpCTVhRZUY1NnNvRkNDSk5jOEdvMU5XOThiRjc=");
+
+            return requestMessage;
         }
 
         protected override void Dispose(bool disposing)
@@ -106,7 +167,7 @@ namespace VPNPlugin
             if (disposing)
             {
                 ServerApi.Hooks.GameInitialize.Deregister(this, OnInitialize);
-                ServerApi.Hooks.ServerJoin.Deregister(this, OnJoinAsync);
+                ServerApi.Hooks.ServerJoin.Deregister(this, OnJoin);
             }
 
             base.Dispose(disposing);
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # AntiVPN
-A [TShock](https://github.com/Pryaxis/TShock) plugin that aims to prevent VPN connections using [IP Intelligence](https://getipintel.net/)'s API services.
+A [TShock](https://github.com/Pryaxis/TShock) plugin that aims to prevent VPN connections using [ipdata](https://getipintel.net/) and [IPHub](https://iphub.info/)'s API services.
 
 **This plugin isn't intended for public use until I un-hardcode stuff** (is that a term?). If you currently attempt to use it on your own server, I am not responsible for rate limit issues you run into.
 
@@ -7,6 +7,12 @@ A [TShock](https://github.com/Pryaxis/TShock) plugin that aims to prevent VPN co
 Place `AntiVPN.dll` into your `ServerPlugins` folder. If all goes well, you should see during TShock initialization.
 Each time a VPN connection is detected it will be printed to TShock's console along with possible errors.
 
-There are currently two subcommands:
- * `status` - Prints the current AntiVPN state; 'on' or 'off.' Mostly for peace of mind.
- * `toggle` - Toggles AntiVPN. Default state is 'on.'
+`/antivpn` will list subcommands. You can allow a TShock group to utilize this command by adding the `antivpn` permission to it.
+
+By default AntiVPN will be enabled at server start and will function every time a player joins, using the ipdata API to check their IP. Once the rate limit is hit on ipdata,
+IPHub will be used instead until the same occurs. **It is unlikely both APIs would be rate limited at any given time, though the possibility exists.**
+
+### There are currently three subcommands:
+ * `status` - Prints the current AntiVPN state (enabled/disabled) and the API being used.
+ * `switchapi` - Allows manual switching of APIs. Might be useful if a certain API is letting VPN connections through (which I've observed).
+ * `toggle` - Disables and re-enables AntiVPN.

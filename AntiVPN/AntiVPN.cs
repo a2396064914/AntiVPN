25391b24b35733b0d074a3012e123f308fba08ba
numanumaa <jacobaustin528@gmail.com>
2022-03-12 13:32:24 -0500

    forgot a single digit.

--- AntiVPN/AntiVPN.cs
+++ AntiVPN/AntiVPN.cs
@@ -15,7 +15,7 @@ namespace VPNPlugin
 
         public override string Author => "numanuma";
 
-        public override Version Version => new Version(1, 0);
+        public override Version Version => new Version(1, 1);
 
         static readonly string[] ignoredIPs = { "10.", "127", "172.16.", "172.17.", "172.18.", "172.19.", "172.20.", "172.21.", "172.22.", "172.23.",
                                                 "172.24.", "172.25.", "172.26", "172.27", "172.28", "172.29", "172.30", "172.31", "192.168"};
